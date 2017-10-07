c219667670ede0564797a97a3bda5e2b77d6c2ae
YasirGulec <yasirgulec@hotmail.com>
2017-10-07 21:37:16 +0300

    Last changes

--- src/test/java/com/gittigidiyor/page/GittiGidiyorHomePage.java
+++ src/test/java/com/gittigidiyor/page/GittiGidiyorHomePage.java
@@ -1,5 +1,28 @@
 package com.gittigidiyor.page;
 
-public class GittiGidiyorHomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class GittiGidiyorHomePage {
+	private WebDriver driver ;
+	private WebDriverWait wait ;
+	private static final String URL = "https://www.gittigidiyor.com/";
+	private static final String SINGIN_BUTTON_ID ="H-Login";
+	
+	
+	public GittiGidiyorHomePage(WebDriver driver) {
+		this.driver=driver;
+		this.wait= new WebDriverWait(this.driver,30); 
+		this.driver.get(URL);
+	}
+	public void clickLoginButton() {
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id(SINGIN_BUTTON_ID))));
+		driver.findElement(By.id(SINGIN_BUTTON_ID)).click();
+	}
+	
+	
+	
+	
 }
